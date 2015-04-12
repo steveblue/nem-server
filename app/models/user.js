f80baf2ad923b7ba3edde4525fec4d6a0b02e8d0
Steve Belovarich <steve@installationspace.com>
2015-04-12 03:16:27 -0700

    Fixed User login, now uses hashed passwords.

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var crypto = require('crypto');
+var bcrypt = require('bcrypt-nodejs');
 var Schema = mongoose.Schema;
 
 /**
@@ -18,55 +19,39 @@ var Schema = mongoose.Schema;
 var UserSchema = new Schema({
     firstName: String,
     lastName: String,
-    username: { type:String, required:true , unique:true},
-    hashed_password: {type:String, required:true},
-    salt: {type:String, required:true},
-    email: String,
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
     lastUpdated : Date,
     created: Date
 });
 
-var validatePresenceOf = function(value) {
-    return value && value.length;
-}
 
-UserSchema.virtual('password')
-    .set(function(password) {
-        this._password = password;
-        this.salt = this.makeSalt();
-        this.hashed_password = this.encryptPassword(password);
-    })
-    .get(function() { return this._password; });
-
-UserSchema.method('authenticate', function(plainText) {
-    console.log('authenticate called:')
-    console.log('plain text = ' + plainText)
-    console.log('hashed = ' + this.encryptPassword(plainText))
-    console.log('db password= ' + this.hashed_password)
-    return this.encryptPassword(plainText) === this.hashed_password;
-});
-
-UserSchema.method('makeSalt', function() {
-    return Math.round((new Date().valueOf() * Math.random())) + '';
-});
-
-UserSchema.method('encryptPassword', function(password) {
-    return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
-});
-
-UserSchema.method('generateToken', function() {
-    return crypto.createHash('md5').update(this.username + Date().toString()).digest("hex");
-});
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 UserSchema.pre('save', function(next) {
+  var user = this;
+  var SALT_FACTOR = 5;
 
-    this.token = this.generateToken();
+  if (!user.isModified('password')) return next();
 
-    if (!validatePresenceOf(this.password || this.hashed_password)) {
-        next(new Error('Invalid password'));
-    } else {
-        next();
-    }
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
 });
+
 
 module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -60,23 +60,19 @@ passport.deserializeUser(function(id, done) {
   });
 });
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) { return done(err); }
-
-      // need to create method on model that handles passwords and auth
-
-      // if (!user) {
-      //   return done(null, false, { message: 'Incorrect username.' });
-      // }
-      // if (!user.validPassword(password)) {
-      //   return done(null, false, { message: 'Incorrect password.' });
-      // }
-      return done(null, user);
+passport.use(new LocalStrategy(function(username, password, done) {
+  User.findOne({ username: username }, function(err, user) {
+    if (err) return done(err);
+    if (!user) return done(null, false, { message: 'Incorrect username.' });
+    user.comparePassword(password, function(err, isMatch) {
+      if (isMatch) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
     });
-  }
-));
+  });
+}));
 
 
 // =========================================================================
